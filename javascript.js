b9c00822c186baf6084ead739b2113bfbced0d71
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-02-23 17:14:53 -0500

    gave birth

--- javascript.js
+++ javascript.js
@@ -3,27 +3,27 @@ var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 var playerPopped = [];
 var computerPopped = [];
 
+
+
 var game = {
   deck: [],
-  player: "John",
+  player: [],
   hands: [],
   playerDeck: [],
   computerDeck: [],
 
 
-
-
   buildDeck: function(){
     values.forEach(function(cardValue){
-        var cardWorth =  values.indexOf(cardValue);
+      var cardWorth =  values.indexOf(cardValue);
 
-        suits.forEach(function(cardSuit){
-            var playingCard= {
-                value: cardValue,
-                suit: cardSuit,
-                worth: cardWorth,
-            };
-            game.deck.push(playingCard);
+      suits.forEach(function(cardSuit){
+        var playingCard= {
+          value: cardValue,
+          suit: cardSuit,
+          worth: cardWorth,
+        };
+        game.deck.push(playingCard);
 
       });//bracker close cardSuit.
     });
@@ -32,57 +32,84 @@ var game = {
   shuffleDeck: function(){
     game.deck.sort(function(){
       var randomValue = Math.random();
-        if (randomValue > 0.5){
-          return 1;
-        }
-          else{
-            return -1;
-          }
+      if (randomValue > 0.5){
+        return 1;
+      }
+      else{
+        return -1;
+      }
     });
   },
 
   buildHands: function(){
-      var playerDeal = this.deck.slice(0, 26);
-      var computerDeal = this.deck.slice(26,52);
-        this.playerDeck = playerDeal;
-        this.computerDeck = computerDeal;
-        console.log(this.playerDeck);
-        console.log(this.computerDeck);
+    var playerDeal = this.deck.slice(0, this.deck.length / 2);
+    var computerDeal = this.deck.slice(this.deck.length / 2,this.deck.length);
+
+    console.log("Player Deal " + playerDeal.length);
+    console.log("Computer Deal " + computerDeal.length);
+    this.playerDeck = playerDeal;
+    this.computerDeck = computerDeal;
+    console.log(this.playerDeck);
+    console.log(this.computerDeck);
   },
 
-
-//push an object into 'hands', object must contain player name and cards. object is name: player, cards: [] in hands
-
-    play: function(){
-      var playerPopped = this.playerDeck.pop();
-      var computerPopped = this.computerDeck.pop();
-
-          if (playerPopped > computerPopped){
-            console.log("Player wins");
-            console.log(playerPopped);
-            console.log(computerPopped);
-            game.playerDeck.push(playerPopped, computerPopped);
-
-          } else {
-            console.log("Computer wins");
-            console.log(playerPopped);
-            console.log(computerPopped);
-            game.playerDeck.push(playerPopped, computerPopped);
-            }
-      console.log(this.computerDeck.length);
+  play: function(){
+    var playerPopped = this.playerDeck.pop();
+    var computerPopped = this.computerDeck.pop();
+    if (playerPopped.worth > computerPopped.worth){
+      showMe("Player wins");
+      console.log(playerPopped);
+      console.log(computerPopped);
+      game.playerDeck.unshift(playerPopped);
+      game.playerDeck.unshift(computerPopped);
       console.log(this.playerDeck.length);
-      // display who won, and show the cards as an alert maybe
+      console.log(this.computerDeck.length);
 
+    }
+    else if(playerPopped.worth == computerPopped.worth){
+      console.log("its a tie!");
+      //eventually play war here//
     }
 
 
-  };
+     else {
+      showMe("Computer wins");
+      console.log(playerPopped);
+      console.log(computerPopped);
+      game.computerDeck.unshift(computerPopped);
+      game.computerDeck.unshift(playerPopped);
+      console.log("computer wins with", computerPopped);
+      console.log("player loses with", playerPopped);
 
+    }
+}
+};
 
 game.buildDeck();
 game.shuffleDeck();
 game.buildHands();
-do{
-  game.play();
+
+var button= document.querySelector("button");
+button.addEventListener("click", function() {
+  for (var i = 0; i <100000; i++){
+
+
+  if(game.playerDeck.length === 0 || game.computerDeck.length === 0){
+    console.log("game over");
+    return;
+
 }
-while(game.playerDeck.length>0 && game.computerDeck.length>0);
+  else {
+    game.play();
+  }
+}
+console.log("done");
+});
+
+
+
+function showMe(message){
+  var div = document.getElementById('message');
+  div.textContent= message;
+  div.style.color= "red";
+}
